810548352b8e7492a3c268273c1185bbc4e7ac31
felipeacsantos <santos.felipe92@gmail.com>
2016-10-12 23:14:17 +0000

    Created CRUD for types

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :types
   resources :organizations
   resources :locals
   resources :roles
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012231053) do
+ActiveRecord::Schema.define(version: 20161012231339) do
 
   create_table "locals", force: :cascade do |t|
     t.string   "name"
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 20161012231053) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
